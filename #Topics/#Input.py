fac35ad49bfe1e4f1132b81d584d4a9f8329cfcd
Prazwol Stark <prazwolstark@gmail.com>
2022-05-08 14:32:24 +0545

    Update #Input.py

---
+++ #Topics/#Input.py
@@ -0,0 +1,9 @@
+# We can input info from user by using input() function
+# Similarly, int() can be used along with input() to enter integer value from the user.
+#while loop can also be used in place of for loop and its syntax is:
+#while condition:
+#statements
+#We can use flag idea to end the loop or programs at various parts.
+#Similarly, break can be used to exit a loop!
+#continue is used for leaving for statments to be executed in a loop and moving to the loop part.
+
