bab7f86351288faf1a0f0008c299af24cb0cfc70
Prazwol Stark <prazwolstark@gmail.com>
2022-05-20 03:59:41 +0545

    55/20

---
+++ #Exercises/#8.3,4,5Exercuses.py
@@ -0,0 +1,10 @@
+#Exercise 8.3
+def shirt_print(size,message):
+    print('You have selected "'+size+'" size of shirt. \n THe message to be printed is:')
+    print('\n'+message)
+shirt_print('Medium','Messi The GOAT')
+print('\n\n')
+shirt_print(message='Ronaldo is an Asshole',size='Large')
+
+#Exercise 8.4
+
