255b70fe01c8f3d98076bb8796c3811c9b02f4d0
Prazwol Stark <prazwolstark@gmail.com>
2022-05-21 23:52:33 +0545

    5/21 .2

---
+++ #Exercises/#8.3,9,10,11,12,13,14,15,1Exercuses.py
@@ -0,0 +1,48 @@
+#Exercise 8.3
+def shirt_print(size,message):
+    print('You have selected "'+size+'" size of shirt. \n THe message to be printed is:')
+    print('\n'+message)
+shirt_print('Medium','Messi The GOAT')
+print('\n\n')
+shirt_print(message='Ronaldo is an Asshole',size='Large')
+
+#Exercise 8.11
+new_list=[]
+def show_magicians(magicians):
+    for magician in magicians:
+        print('-'+magician)
+def make_great(magicians):
+    for magician in magicians:
+        magician= 'The Great' + magician
+        new_list.append(magician)
+    return new_list
+
+magicians=['Jonathan Todd','Michael Carbonaro','Chris Angel','Siegfried & Roy']
+magicians = make_great(magicians)
+show_magicians(magicians)
+
+#Exercise 8.12
+sandwiches=[]
+def make_sandwich(sandwiches):
+    print('\n\nThe items in your Sandwiche are: \n')
+    for sandwich in sandwiches:
+        print('-'+ sandwich)
+def ask_customer():
+    print('What would you want in you sandwich sir?')
+    print('Note: Please, Enter the items followed by Enter key after each items and when finished press enter on blank line! ')
+    while True:
+        topping = input()
+        if topping:
+            sandwiches.append(topping)
+        else:
+            break
+    return sandwiches
+sandwiches = ask_customer()
+make_sandwich(sandwiches)
+
+
+#Exercise 8.13
+
+
+
+
