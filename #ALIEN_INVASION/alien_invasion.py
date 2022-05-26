c2107a40207a7429247ef937ad7e7c42a41e97b4
Prazwol Stark <prazwolstark@gmail.com>
2022-05-26 23:58:58 +0545

    5.26

--- #ALIEN_INVASION/alien_invasion.py
+++ #ALIEN_INVASION/alien_invasion.py
@@ -21,7 +21,6 @@ def run_game():
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         bullets.update()
-        gf.update_screen(ai_settings, screen, ship,bullets)
-
+        gf.update_screen(ai_settings,screen,ship,bullets)
 run_game()
 
