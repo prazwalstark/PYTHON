4578cc1d249737ed7d8e76e0752f355975c23930
Prazwol Stark <prazwolstark@gmail.com>
2022-05-27 00:17:55 +0545

    5.26 games

--- #ALIEN_INVASION/bullet.py
+++ #ALIEN_INVASION/bullet.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Bullet (Sprite):
+class Bullet(Sprite):
     '''A class to manage bullets fired from the ship'''
     def __init__ (self, ai_settings, screen,ship):
         '''Create a bullet object at the ship's current position.'''
--- #ALIEN_INVASION/game_functions.py
+++ #ALIEN_INVASION/game_functions.py
@@ -22,19 +22,20 @@ def check_keyup_events(event,ship):
 def check_events(ai_settings,screen,ship,bullets):
     '''Respond to keypress and mouse events.'''
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                check_keydown_events(event,ai_settings,screen,ship,bullets)
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event,ai_settings,screen,ship,bullets)
 
-            elif event.type == pygame.KEYUP:
-                check_keyup_events(event,ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event,ship)
 
 def update_screen(ai_settings,screen,ship,bullets):
     '''Update images on the screen and flip to the new screen.'''
     screen.fill(ai_settings.screen_bg_color)
-    ship.blitme()
-    pygame.display.flip()
     #Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
+    ship.blitme()
+    pygame.display.flip()
+    
