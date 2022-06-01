f0e2e338eb724cc885a546b07d465612e2b0a686
Prazwol Stark <prazwolstark@gmail.com>
2022-06-01 10:46:12 +0545

    6/1

--- #ALIEN_INVASION/alien_invasion.py
+++ #ALIEN_INVASION/alien_invasion.py
@@ -4,7 +4,6 @@ from pygame.sprite import Group
 
 from setting import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -13,18 +12,21 @@ def run_game():
     ai_settings= Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion -Prazwal D. Stark")
+    #Make a ship ,a group of bullets, and a group of aliens.
     ship = Ship(ai_settings,screen)
     #Make a groupto store bullets in.
     bullets = Group()
-    #Make an alien.
-    alien = Alien(ai_settings,screen)
+    aliens = Group()
+
+    #Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen,ship, aliens)
 
     #Start the main loop for the game.
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,alien,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
         
 run_game()
 
